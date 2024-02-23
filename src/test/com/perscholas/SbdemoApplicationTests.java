db78a2a290c54f22aee9be6c0899e28b1814d0d0
simon mekit <simonmekit@gmail.com>
2024-02-23 11:33:14 -0500

    initial commit

---
+++ src/test/com/perscholas/SbdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package src.test.com.perscholas;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SbdemoApplicationTests {
+
+	@Test
+    public void contextLoads() {
+	}
+
+}
